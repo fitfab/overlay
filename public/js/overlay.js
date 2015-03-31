a43983270873de90066a14f0258ff966e8cbd283
Miguel Julio <fitfab@yahoo.com>
2015-03-30 22:40:25 -0400

    setting the overlay to sroll

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,15 +1,14 @@
 /* SmartOverlay Creates Constructor */
 var SmartOverlay = function(map) {
-  var $win = $(window);
+  $win = $(window);
+  this.$body = $('body');
   this.$iframe = $('<iframe id="modal-iframe" />');
   this.defaults = { width: 780, height: 'auto', background: '#fff'};
-  this.$bg = $( '<div class="opaque transit"></div>' ).hide().appendTo('body');
-  this.$el = $('<div class="modal transit"><b class="modal-close"></b><div class=loader></div></div>')
-              .append('<div class="modal-content"></div>');
-
+  this.$bg = $(this.htmlWrapper()).hide().appendTo('body');
+  this.$el = $(this.htmlModal());
+              
   // Listen for call to the overlay from a click event
   $win.on('click', function(e){
-    e.preventDefault();
     if ( $(e.target).hasClass('overlayTrigger') ){
       var options = $(e.target).data("dialog");
       this.open(map[options]);
@@ -36,7 +35,7 @@ var SmartOverlay = function(map) {
 
     // Iframe is ready
     if(e.originalEvent.data === 'iframe:ready'){
-      this.$el.addClass('loaded');
+      this.$el.find('.modal').addClass('loaded');
       return;
     };
 
@@ -48,32 +47,46 @@ var SmartOverlay = function(map) {
 
   }.bind(this));
 
-  this.$el.appendTo('body');
+  //this.$el.appendTo('body');
+  this.$bg.append(this.$el);
 };
 
 /* SmartOverlay Prototype */
 SmartOverlay.prototype = {
+  htmlWrapper : function(){
+    return '<div class="modal-wrapper"><div class="opaque"></div></div>';
+  },
+
+  htmlModal : function(){
+    var str = '<div class="modal-viewport"><div class="modal transit">'
+            + '<b class="modal-close"></b><div class=loader></div>'
+            + '<div class="modal-content"></div></div></div>';
+    return str;
+  },
 
   open : function( options) {
     var settings = $.extend( {}, this.defaults, options.prop );
+    // set body to overflow hidden
+    this.$body.addClass('modal-on');
     // Shows the background
     this.$bg.show().addClass('open');
 
     if( options.iframe && options.url) {
       options.content = this.$iframe.attr("src", options.url);
     } else {
-      this.$el.addClass('loaded');
+      this.$el.find('.modal').addClass('loaded');
     };
 
     this.$el.find('.modal-content').html(options.content);
-    this.$el.addClass('open');
+    
 
     // apply styles to the modal
-    this.$el.css(settings);
+    this.$el.find('.modal').css(settings).addClass('open');;
   },
 
   closeModal : function(){
-    this.$el.removeClass('open loaded');
+    this.$body.removeClass('modal-on');
+    this.$el.find('.modal').removeClass('open loaded');
     this.$bg.removeClass('open').hide();
   }
 };
